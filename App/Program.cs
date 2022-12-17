9b056d97f3d633b0f9ae8ea50594a99afe3337cd
Jessica Ward <Jessicawardiom@gmail.com>
2022-12-17 15:08:15 +0000

    It's ChugTime

---
+++ App/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading;
+
+var max = args.Length != 0 ? Convert.ToInt32(args[0]) : int.MaxValue;
+for(var x = 0; x < max; x++)
+{
+    System.Console.WriteLine("Chug!");
+    Thread.Sleep(500);
+}
